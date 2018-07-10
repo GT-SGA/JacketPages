480e1a4e6773d50f658564f4628ad29e114ae39d
Jon King <jwking2112@gmail.com>
2018-07-10 11:55:05 -0400

    Added credentials to app.js

--- app.js
+++ app.js
@@ -2,18 +2,32 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var mysql = require('mysql')
+const bodyParser = require('body-parser')
 
 
 var connection = mysql.createConnection({
-  host     : '',
-  user     : '',
-  password : '',
-  database : ''
+  host     : 'locahost',
+  user     : 'root',
+  password : 'password',
+  database : 'jpdev'
 });
+
+connection.connect();
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/organizations.html'));
 });
 
-app.listen(8080);
+connection.query('SELECT * from users', function(err, rows, fields) {
+  if (!err)
+    console.log('The solution is: ', rows);
+  else
+    console.log('Error while performing Query.');
+});
+
+connection.end();
+
+app.listen(8080, () => {
+  console.log('Server running on http://localhost:8080')
+})
