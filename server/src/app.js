765466e60292435b5c6193091ddc55f802fbb43a
lastgituser <akikkeri3@gatech.edu>
2018-07-15 19:09:33 -0400

    Implemented most of creating bills

--- server/src/app.js
+++ server/src/app.js
@@ -162,3 +162,19 @@ app.put("/bill_sign_vf", (req, res) => {
         });
     });
 });
+
+// Create a new bill
+// Still prone to SQL injection attacks.
+app.post("/bill_create", (req, res) => {
+    connection.query(`INSERT INTO bill_authors (undr_auth_id, grad_auth_id) VALUES 
+            ('${req.param("data_Authors_undr_auth_id")}', '${req.param("data_Authors_grad_auth_id")}')`, function(err, rows) {
+        console.log(JSON.stringify(req.param("data_Authors_undr_auth_id")));
+        connection.query(`INSERT INTO bills (create_date, last_mod_date, title, fundraising, type, category, org_id, dues, ugMembers, gMembers, auth_id) VALUES 
+                (NOW(), NOW(),'${req.param("data_Bill_title")}', '${req.param("data_Bill_fundraising")}', '${req.param("data_Bill_type")}', 
+                '${req.param("data_Bill_category")}', '${req.param("data_Bill_org_id")}', '${req.param("data_Bill_dues")}', '${req.param("data_Bill_ugMembers")}', 
+                '${req.param("data_Bill_gMembers")}', '${rows.insertId}')`,
+                function(err, rows) {
+            res.send("");
+        });
+    });
+});
