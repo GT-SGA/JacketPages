9d8315def3b858d8081f3421fe43ded5b6eda0a8
lastgituser <akikkeri3@gatech.edu>
2018-07-25 02:55:48 -0400

    Fixed line items issue and changed all req.params to req.query

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ const path = require("path");
 const mysql = require("mysql");
 const app = express();
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 ////////////////////////////////////////////////////////////////////////////////  SERVER & DB SETUP 
 
 // Connect to the MySQL database
 var connection = mysql.createConnection({
     host        :   "localhost",
-    user        :   "jpdev",
-    password    :   "jpdev!"
+    user        :   "localuser",
+    password    :   "password"
 });
 
 connection.connect(function(err) {
@@ -49,7 +50,7 @@ app.get('/sgapeople', function(req, res){    // SGAPEOPLE
 
 ////////////////////////////////////////////////////////////////////////////////  SGA PEOPLE 
 app.get("/user", (req, res) => {
-    connection.query(`SELECT * FROM users WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`SELECT * FROM users WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -57,7 +58,7 @@ app.get("/user", (req, res) => {
 });
 
 app.get("/person", (req, res) => {
-    connection.query(`SELECT * FROM sga_people WHERE user_id=${req.param("user_id")}`, function(err, rows) {
+    connection.query(`SELECT * FROM sga_people WHERE user_id=${req.query["user_id"]}`, function(err, rows) {
         res.send({data:
             rows
         });
@@ -78,7 +79,7 @@ app.get("/people", (req, res) => {
 // Return the data of the bills submitted by a certain submitter
 // Still prone to SQL injection attacks.
 app.get("/bills_sub", (req, res) => {
-    connection.query(`SELECT * FROM bills WHERE submitter=${req.param('submitter')}`, function(err, rows) {
+    connection.query(`SELECT * FROM bills WHERE submitter=${req.query['submitter']}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -89,7 +90,7 @@ app.get("/bills_sub", (req, res) => {
 // Still prone to SQL injection attacks.
 app.get("/bill_id", (req, res) => {
     // Return all bills belonging to a certain submitter. Still prone to SQL injection attacks.
-    connection.query(`SELECT * FROM bills WHERE id=${req.param('id')}`, function(err, rows) {
+    connection.query(`SELECT * FROM bills WHERE id=${req.query['id']}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -100,7 +101,7 @@ app.get("/bill_id", (req, res) => {
 // Still prone to SQL injection attacks.
 app.get("/bill_votes", (req, res) => {
     // Return all bills belonging to a certain submitter. Still prone to SQL injection attacks.
-    connection.query(`SELECT * FROM bill_votes WHERE id=${req.param('id')}`, function(err, rows) {
+    connection.query(`SELECT * FROM bill_votes WHERE id=${req.query['id']}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -111,7 +112,7 @@ app.get("/bill_votes", (req, res) => {
 // Still prone to SQL injection attacks.
 app.get("/org", (req, res) => {
     // Return all bills belonging to a certain submitter. Still prone to SQL injection attacks.
-    connection.query(`SELECT * FROM organizations WHERE id=${req.param('id')}`, function(err, rows) {
+    connection.query(`SELECT * FROM organizations WHERE id=${req.query['id']}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -121,16 +122,16 @@ app.get("/org", (req, res) => {
 // Delete the bill by a certain id
 // Still prone to SQL injection attacks.
 app.delete("/bill_id", (req, res) => {
-    connection.query(`DELETE FROM bills WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`DELETE FROM bills WHERE id=${req.query["id"]}`, function(err, rows) {
         if (err) res.send({err: err});
-        else res.send(`id ${req.param("id")} deleted!`);
+        else res.send(`id ${req.query["id"]} deleted!`);
     });
 });
 
 // Get status for a certain bill given id
 // Still prone to SQL injection attacks.
 app.get("/bill_status", (req, res) => {
-    connection.query(`SELECT name FROM bill_statuses WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`SELECT name FROM bill_statuses WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -140,7 +141,7 @@ app.get("/bill_status", (req, res) => {
 // Get user given id
 // Still prone to SQL injection attacks.
 app.get("/user", (req, res) => {
-    connection.query(`SELECT * FROM users WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`SELECT * FROM users WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -150,7 +151,7 @@ app.get("/user", (req, res) => {
 // Get submitter for a certain bill given id
 // Still prone to SQL injection attacks.
 app.get("/bill_authors", (req, res) => {
-    connection.query(`SELECT * FROM bill_authors WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`SELECT * FROM bill_authors WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -160,7 +161,7 @@ app.get("/bill_authors", (req, res) => {
 // Get submitter for a certain bill given id
 // Still prone to SQL injection attacks.
 app.put("/bill_passed", (req, res) => {
-    connection.query(`UPDATE bills SET status=6 WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`UPDATE bills SET status=6 WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -170,7 +171,7 @@ app.put("/bill_passed", (req, res) => {
 // Approve a bill for the graduate president
 // Still prone to SQL injection attacks.
 app.put("/bill_sign_gp", (req, res) => {
-    connection.query(`UPDATE bill_authors SET grad_pres_id=${req.param("gp_id")} WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`UPDATE bill_authors SET grad_pres_id=${req.query["gp_id"]} WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -180,7 +181,7 @@ app.put("/bill_sign_gp", (req, res) => {
 // Approve a bill for the graduate secretary
 // Still prone to SQL injection attacks.
 app.put("/bill_sign_gs", (req, res) => {
-    connection.query(`UPDATE bill_authors SET grad_secr_id=${req.param("gs_id")} WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`UPDATE bill_authors SET grad_secr_id=${req.query["gs_id"]} WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -190,7 +191,7 @@ app.put("/bill_sign_gs", (req, res) => {
 // Approve a bill for the undergraduate president
 // Still prone to SQL injection attacks.
 app.put("/bill_sign_up", (req, res) => {
-    connection.query(`UPDATE bill_authors SET undr_pres_id=${req.param("up_id")} WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`UPDATE bill_authors SET undr_pres_id=${req.query["up_id"]} WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -200,7 +201,7 @@ app.put("/bill_sign_up", (req, res) => {
 // Approve a bill for the undergraduate secretary
 // Still prone to SQL injection attacks.
 app.put("/bill_sign_us", (req, res) => {
-    connection.query(`UPDATE bill_authors SET undr_secr_id=${req.param("us_id")} WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`UPDATE bill_authors SET undr_secr_id=${req.query["us_id"]} WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -210,7 +211,7 @@ app.put("/bill_sign_us", (req, res) => {
 // Approve a bill for the vice president of finance
 // Still prone to SQL injection attacks.
 app.put("/bill_sign_vf", (req, res) => {
-    connection.query(`UPDATE bill_authors SET vp_fina_id=${req.param("vf_id")} WHERE id=${req.param("id")}`, function(err, rows) {
+    connection.query(`UPDATE bill_authors SET vp_fina_id=${req.query["vf_id"]} WHERE id=${req.query["id"]}`, function(err, rows) {
         res.send({data: 
             rows
         });
@@ -221,22 +222,28 @@ app.put("/bill_sign_vf", (req, res) => {
 // Still prone to SQL injection attacks.
 app.post("/bill_create", (req, res) => {
     connection.query(`INSERT INTO bill_authors (undr_auth_id, grad_auth_id) VALUES 
-            ('${req.param("data_Authors_undr_auth_id")}', '${req.param("data_Authors_grad_auth_id")}')`, function(err, rows) {
+            ('${req.query["data_Authors_undr_auth_id"]}', '${req.query["data_Authors_grad_auth_id"]}')`, function(err, rows) {
         connection.query(`INSERT INTO bills (create_date, last_mod_date, title, description, fundraising, type, category, org_id, dues, ugMembers, gMembers, auth_id, status) VALUES 
-                (NOW(), NOW(), '${req.param("data_Bill_title")}', '${req.param("data_Bill_description")}', '${req.param("data_Bill_fundraising")}', '${req.param("data_Bill_type")}', 
-                '${req.param("data_Bill_category")}', ${req.param("data_Bill_org_id")}, '${req.param("data_Bill_dues")}', '${req.param("data_Bill_ugMembers")}', 
-                ${req.param("data_Bill_gMembers")}, ${rows.insertId}, 1)`,
+                (NOW(), NOW(), '${req.query["data_Bill_title"]}', '${req.query["data_Bill_description"]}', '${req.query["data_Bill_fundraising"]}', '${req.query["data_Bill_type"]}', 
+                '${req.query["data_Bill_category"]}', ${req.query["data_Bill_org_id"]}, '${req.query["data_Bill_dues"]}', '${req.query["data_Bill_ugMembers"]}', 
+                ${req.query["data_Bill_gMembers"]}, ${rows.insertId}, 1)`,
                 function(err, rows) {
             let billId = rows.insertId;
-            for (let i = 0; req.param(`data_${i}_LineItem_line_number`); i++) {
+            for (let i = 0; req.query[`data_${i}_LineItem_line_number`]; i++) {
+                let line_number = req.query[`data_${i}_LineItem_line_number`];
+                let name = req.query[`data_${i}_LineItem_name`];
+                let cost_per_unit = req.query[`data_${i}_LineItem_cost_per_unit`];
+                let quantity = req.query[`data_${i}_LineItem_quantity`];
+                let total_cost = req.query[`data_${i}_LineItem_total_cost`];
+                let amount = req.query[`data_${i}_LineItem_amount`];
+                let account = req.query[`data_${i}_LineItem_account`];
+                let type = req.query[`data_${i}_LineItem_type`];
+                let comments = req.query[`data_${i}_LineItem_comments`];
                 connection.query(`INSERT INTO line_items (line_number, bill_id, name, cost_per_unit, quantity, total_cost, amount, account, type, comments, last_mod_date) VALUES
-                        (${req.param('data_${i}_LineItem_line_number')}, ${billId}, ${req.param('data_${i}_LineItem_name')}, ${req.param('data_${i}_LineItem_cost_per_unit')}, 
-                                ${req.param('data_${i}_LineItem_quantity')}, ${req.param('data_${i}_LineItem_total_cost')}, 
-                                ${req.param('data_${i}_LineItem_amount')}, ${req.param('data_${i}_LineItem_account')}, 
-                                ${req.param('data_${i}_LineItem_type')}, ${req.param('data_${i}_LineItem_comments')}, NOW())`,
+                        (${line_number}, ${billId}, '${name}', ${cost_per_unit}, ${quantity}, ${total_cost}, ${amount}, '${account}', '${type ? type : ""}', ${comments ? comments : null}, NOW())`,
                 function(err, rows) {
                     if (err) console.log(err);
-                    console.log(req.param("data_0_LineItem_line_number"));
+                    //console.log(name);
                 });
             }
             res.send(rows);
