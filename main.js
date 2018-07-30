a7436f971b85464bea8d84a21296c9ae97f6989e
ew2886 <31673898+ew2886@users.noreply.github.com>
2018-07-30 16:28:53 -0400

    lots of functions working now
    add user
    edit user
    deleting user (but page freezes)
    editing sga member
    still need to redirect everything

--- main.js
+++ main.js
@@ -55,25 +55,15 @@ app.get('/sgapeople', function(req, res){    // SGAPEOPLE
 
 //Gets list of all users in the user table 
 app.get("/users", (req, res) => {
-    connection.query(`SELECT * FROM users`, function(err, rows) {
+    connection.query(`SELECT * FROM users ORDER BY id DESC LIMIT 50`, function(err, rows) {
         res.send({data: rows});
     });
 });
 
-//IS THIS STILL NEEDED????????
 //Gets user in user table based on id 
+//used in sgapeople.html to get first and last names 
 app.get("/user", (req, res) => {
     connection.query(`SELECT * FROM users WHERE id=${req.query.id}`, function(err, rows) {
-        res.send({data:
-                 rows
-        });
-    });
-});
-
-//IS THIS STILL NEEDED???????
-//Gets user in user table based on first and last name
-app.get("/person", (req, res) => {
-    connection.query(`SELECT * FROM users WHERE first_name="${req.query.first_name}" and last_name="${req.query.last_name}"`, function(err, rows) {
         res.send({data:
                  rows
         });
@@ -107,22 +97,39 @@ app.get("/sga_member", (req, res) => {
     });
 }); 
 
+//deletes a user from user table using their id passed in through the url
+app.get("/delete_user", (req, res) => {
+    //debugging
+    res.write('You deleted the user with the id "' + req.query.id+'".\n');
+    
+    connection.query(`DELETE FROM users WHERE id=${req.query.id}`, function(err, rows) {
+        if (err) console.log(err); 
+    });
+});
+
+//deletes a member from sga_people table using their user_id passed in through the url
+app.get("/delete_member", (req, res) => {
+    //debugging
+    res.write('You deleted the sga member with the user_id "' + req.query.user_id+'".\n');
+    
+    connection.query(`DELETE FROM sga_people WHERE id=${req.query.user_id}`, function(err, rows) {
+        if (err) console.log(err); 
+    });
+});
+
+//edits a user from user table using their id passed in through the url 
 app.post("/edit_user", (req, res) => {
     //debugging
-    console.log(req.data);
     res.write('You sent the name "' + req.body.firstname+'".\n');
-    res.write('You sent the house "' + req.body.house+'".\n');
-    res.write('You sent the department "' + req.body.department+'".\n');
+    res.write('You sent the level"' + req.body.level+'".\n');
+    res.write('You sent the email"' + req.body.email+'".\n');
+    res.write('You sent the major"' + req.body.major+'".\n');
+    res.write('You sent the year"' + req.body.year+'".\n');
     res.end()
     
-    connection.query(`UPDATE users SET first_name='${req.body.firstname}', last_name='${req.body.lastname}' WHERE id=${req.query.id}`, function(err, rows) {
+    connection.query(`UPDATE users SET email='${req.body.email}', first_name='${req.body.firstname}', last_name='${req.body.lastname}', level='${req.body.level}', major='${req.body.major}', year='${req.body.year}' WHERE id=${req.query.id}`, function(err, rows) {
         if (err) console.log(err); 
     });
-    
-    connection.query(`UPDATE sga_people SET house='${req.body.house}', department='${req.body.department}' WHERE user_id=${req.query.id}`, function(err, rows) {
-        if (err) console.log(err); 
-    });
-    
 });
 
 //edits a sga member using their id in the user table and user id in the sga_people table 
@@ -134,17 +141,14 @@ app.post("/edit_member", (req, res) => {
     res.write('You sent the department "' + req.body.department+'".\n');
     res.end()
     
-    connection.query(`UPDATE users SET first_name='${req.body.firstname}', last_name='${req.body.lastname}' WHERE id=${req.query.id}`, function(err, rows) {
-        if (err) console.log(err); 
-    });
-    
-    connection.query(`UPDATE sga_people SET house='${req.body.house}', department='${req.body.department}' WHERE user_id=${req.query.id}`, function(err, rows) {
+    connection.query(`UPDATE sga_people SET house='${req.body.house}', department='${req.body.department}', status='${req.body.status}' WHERE user_id=${req.query.id}`, function(err, rows) {
         if (err) console.log(err); 
     });
     
 });
  
 //adds a new user into the users table 
+//make sure major and year columns are added to database 
 app.post("/add_user", (req, res) => {
     
     //debugging
@@ -155,7 +159,8 @@ app.post("/add_user", (req, res) => {
     res.write('You sent the year "' + req.body.year+'".\n');
     res.end()
     
-    //id is passed in through the url 
+    //id is passed in through the url
+    //in usersadd, ajax gets id of most recently added user than adds 1 to it. Passes this number through the url 
     connection.query(`INSERT INTO users (id, gt_user_name, email, first_name, last_name, major, year) VALUES ('${req.query.id}', '${req.body.gtusername}', '${req.body.email}', '${req.body.firstname}', '${req.body.lastname}', '${req.body.major}', '${req.body.year}')`, function(err, rows) {
         if (err) console.log(err); 
     });
