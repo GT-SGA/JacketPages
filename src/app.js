43e76be399c8015d94e74139c7c396f9f4c06939
Emily Wilson <emilywilson6263@gmail.com>
2018-09-08 18:45:04 -0400

    set up the project structure for migrating to react

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import Router from 'react-router';
+
+const App = () => (
+  <div>App</div>
+);
+
+export default hot(module)(App);
---
+++ src/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Home = () => (
+  <div id="content">
+    <div class="clearfix">
+      <div id="middle_full">
+          <div id="page_title">Welcome to JacketPages</div>
+            <p>JacketPages serves as the Student Government Association bills system for financial allocations to student organizations and resolutions to represent the student opinion. If you are looking for organization information, please visit the Georgia Tech Orgsync! JacketPages will continue to be updated more over the next few weeks!</p>
+            <h1>SGA Finance Resources</h1>
+            <div style="margin: auto; width: 85%; padding-top: 10px;">
+              <iframe src="http://sgafinance.atspace.cc/" width="100%" height="100%"></iframe>
+            </div>
+          <p></p>
+      </div>
+    </div>
+  </div>
+);
