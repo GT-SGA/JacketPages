4ddbbb37f1516e36aca6f815e3ce0463c67c6f33
Aljon Pineda <aljonjbpineda@gmail.com>
2018-11-02 10:15:30 -0400

    Fixed style properties

--- client/src/screens/studentgovernment/votinguhrrepview.js
+++ client/src/screens/studentgovernment/votinguhrrepview.js
@@ -4,47 +4,50 @@ class VotingUHRRepView extends Component {
   render() {
     return (
       <div>
-        <div className="container border-bottom" style={{maxWidth: "none"}}>
-            <div className="row">
-                <div className="col">
-                    <p style={{margin: 0, fontSize: 30}}>JacketPages</p>
-                </div>
-                <div className="col d-flex justify-content-end align-items-center">
-                    <p className="text-right" style={{margin: 0}}>Logged in as: UHR Rep</p>
-                </div>
-            </div>
-        </div>
         <div id="wrapper">
-            <div id="sidebar-wrapper" style={{backgroundColor: "rgb(238,178,17)"}}>
-                <ul className="sidebar-nav">
-                    <li className="sidebar-brand"> <a href="#" style={{color: "#212529"}}>Agenda</a></li>
-                    <li> <a href="#" style={{color: "#b98700"}}>Bill 1</a></li>
-                    <li> <a href="#" style={{color: "#212529"}}>Bill 2</a></li>
-                    <li> <a href="#" style={{color: "#212529"}}>Bill 3</a></li>
-                </ul>
-            </div>
-            <div className="page-content-wrapper">
-                <div className="container-fluid"><a className="btn btn-link" role="button" href="#menu-toggle" id="menu-toggle"><i className="fa fa-bars"></i></a>
-                    <div className="row">
-                        <div className="col-md-12 col-lg-12">
-                            <h2>Bill 2</h2>
-                            <h3>Organization:</h3>
-                            <p>Information about bill<br />Info<br />Info<br />Info</p>
-                            <div className="row">
-                                <div className="col">
-                                    <h3>Vote:</h3>
-                                    <div className="row">
-                                        <div className="col d-flex justify-content-center"><button className="btn btn-primary btn-block" type="button" style={{backgroundColor: "rgb(43,227,40)", border: 0}}>Yea</button></div>
-                                        <div className="col d-flex justify-content-center"><button className="btn btn-primary btn-block" type="button" style={{backgroundColor: "#c9c9c9", border: 0}}>Abstain</button></div>
-                                        <div className="col d-flex justify-content-center"><button className="btn btn-primary btn-block" type="button" style={{backgroundColor: "rgb(224,46,34)", border: 0}}>Nay</button></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <div id="sidebar-wrapper" style={{backgroundColor: "rgb(238,178,17)"}}>
+              <ul className="sidebar-nav">
+                  <li className="sidebar-brand"> <a href="#" style={{color: "#212529"}}>Agenda</a></li>
+                  <li> <a href="#" style={{color: "#b98700"}}>Bill 1</a></li>
+                  <li> <a href="#" style={{color: "#212529"}}>Bill 2</a></li>
+                  <li> <a href="#" style={{color: "#212529"}}>Bill 3</a></li>
+              </ul>
+          </div>
+          <div className="page-content-wrapper">
+              <div className="container border-bottom" style={{maxWidth: "none"}}>
+                  <div className="row">
+                      <div className="col">
+                          <p style={{margin: 0, fontSize: 30}}>JacketPages</p>
+                      </div>
+                      <div className="col d-flex justify-content-end align-items-center">
+                          <p className="text-right" style={{margin: 0}}>Logged in as: UHR Rep</p>
+                      </div>
+                  </div>
+              </div>
+              <div className="container-fluid"><a className="btn btn-link" role="button" href="#menu-toggle" id="menu-toggle"><i className="fa fa-bars"></i></a>
+                  <div className="row">
+                      <div className="col-md-12 col-lg-12">
+                          <h2>Bill 2</h2>
+                          <h3>Organization:</h3>
+                          <p>Information about bill<br />Info<br />Info<br />Info</p>
+                          <div className="row">
+                              <div className="col">
+                                  <h3>Vote:</h3>
+                                  <div className="row">
+                                      <div className="col d-flex justify-content-center"><button className="btn btn-primary btn-block" type="button" style={{backgroundColor: "rgb(43,227,40)", border: 0}}>Yea</button></div>
+                                      <div className="col d-flex justify-content-center"><button className="btn btn-primary btn-block" type="button" style={{backgroundColor: "#c9c9c9", border: 0}}>Abstain</button></div>
+                                      <div className="col d-flex justify-content-center"><button className="btn btn-primary btn-block" type="button" style={{backgroundColor: "rgb(224,46,34)", border: 0}}>Nay</button></div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
         </div>
+        <script src="../../assets/js/jquery.min.js"></script>
+        <script src="../../assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../../assets/js/Sidebar-Menu.js"></script>
       </div>
     );
   }
