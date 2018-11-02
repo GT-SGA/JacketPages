75b74f98e4d305a89ef2ba460ee450aa04f981ad
Emily Wilson <emilywilson6263@gmail.com>
2018-11-02 12:25:22 -0400

    Add CAS Authentication GT to Auth Router (#81)
    * add cas setup to maiain.js
    
    * Added auth route
    
    * Add GT Login (CAS) support using SAML (#65)

--- main.js
+++ main.js
@@ -1,53 +1,71 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const path = require("path");
-const mysql = require("mysql");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const mysql = require('mysql');
+const session = require('express-session');
+const CASAuthentication = require('cas-authentication-gt');
+
 const app = express();
 
-
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true,
 }));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//Cors Middleware
+// Cors Middleware
 app.use(cors());
 
-////////////////////////////////////////////////////////////////////////////////  SERVER & DB SETUP
+// Express Session
+app.use(session({
+  secret: 'jpsessionkey',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+const cas = new CASAuthentication({
+  cas_url: 'https://login.gatech.edu/cas/login',
+  service_url: 'https://jacketpages.gatech.edu',
+});
+
+/* SERVER & DB SETUP */
 
 // Connect to the MySQL database
-var connection = mysql.createConnection({
-    host        :   "localhost",
-    user        :   "root",
-    password    :   "password"
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
-// Use the database jacketpages_dev
-connection.query("USE jpdev");
 
-var server = app.listen(process.env.PORT || 5000);   //server listening to localhost 5000
+// Use the database jacketpages_dev
+connection.query('USE jpdev');
+
+const port = (process.env.port || 80);
+const server = app.listen(port);
 
 const io = require('socket.io').listen(server);
 
-app.use(express.static("public"));  //automatically serves static files home.html and its css files
+app.use(express.static('public')); // automatically serves static files home.html and its css files
 
-console.log("Running on port 5000");
+console.log('Running on port ' + port);
 
-//Load Routes
-const users = require("./routes/users");
-const bills = require("./routes/bills");
-const index = require("./routes/index");
+// Load Routes
+const users = require('./routes/users');
+const bills = require('./routes/bills');
+const index = require('./routes/index');
+const auth = require('./routes/auth');
 
 
 //Use Routes
 app.use('/users/api', users);
 app.use('/bills', bills);
-app.use('/api', index)
+app.use('/api', index);
+app.use('/auth', auth);
